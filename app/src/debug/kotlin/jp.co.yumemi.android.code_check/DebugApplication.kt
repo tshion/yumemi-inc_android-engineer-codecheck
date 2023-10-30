07a5d58fc56b304ff1ac801a7ad1384021556b91
tshion <tenta_shion@yahoo.co.jp>
2023-10-30 21:29:38 +0900

    StrictMode の設定

--- app/src/debug/kotlin/jp.co.yumemi.android.code_check/DebugApplication.kt
+++ app/src/debug/kotlin/jp.co.yumemi.android.code_check/DebugApplication.kt
@@ -1,6 +1,29 @@
 package jp.co.yumemi.android.code_check
 
+import android.os.StrictMode
+
 /**
  * Debug ビルド時に適用するApplication クラス
  */
-class DebugApplication : MainApplication()
+class DebugApplication : MainApplication() {
+
+    override fun onCreate() {
+        // region: StrictMode の設定
+        StrictMode.ThreadPolicy.Builder()
+            .detectAll()
+            .penaltyLog()
+            .penaltyFlashScreen()
+            .build()
+            .also { StrictMode.setThreadPolicy(it) }
+
+        StrictMode.VmPolicy.Builder()
+            .detectAll()
+            .penaltyLog()
+            .penaltyDeath()
+            .build()
+            .also { StrictMode.setVmPolicy(it) }
+        // endregion
+
+        super.onCreate()
+    }
+}
