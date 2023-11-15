d46599a5166ffed3dccaa813d619127f7599d389
tshion <tenta_shion@yahoo.co.jp>
2023-11-15 18:55:38 +0900

    エントリーポイントになるActivity の追加

---
+++ app/src/main/kotlin/jp/co/yumemi/android/code_check/EntryPointActivity.kt
@@ -0,0 +1,8 @@
+package jp.co.yumemi.android.code_check
+
+import androidx.appcompat.app.AppCompatActivity
+
+/**
+ * エントリーポイントを担当するActivity
+ */
+class EntryPointActivity : AppCompatActivity(R.layout.entry_point)
