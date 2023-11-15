18bab701ff277e92100c0cdf438f8345c753b59b
tshion <tenta_shion@yahoo.co.jp>
2023-11-15 18:15:46 +0900

    ローディングUI の実装

---
+++ app/src/main/kotlin/jp/co/yumemi/android/code_check/atoms/LoadingOverlay.kt
@@ -0,0 +1,52 @@
+package jp.co.yumemi.android.code_check.atoms
+
+import android.content.Context
+import android.util.AttributeSet
+import android.view.Gravity
+import android.view.ViewGroup.LayoutParams.WRAP_CONTENT
+import android.widget.FrameLayout
+import com.google.android.material.progressindicator.CircularProgressIndicator
+import jp.co.yumemi.android.code_check.R
+
+/**
+ * ローディングUI
+ *
+ * ## 使い方
+ * 表示に時間がかかるView に被せるように配置してください。
+ *
+ * ``` xml
+ * <FrameLayout>
+ *     <!-- ローディングを行っているものを表示するView -->
+ *     <View />
+ *
+ *     <!-- ローディング表示 -->
+ *     <(package).LoadingOverlay />
+ * </FrameLayout>
+ * ```
+ */
+class LoadingOverlay @JvmOverloads constructor(
+    context: Context,
+    attrs: AttributeSet? = null,
+) : FrameLayout(context, attrs) {
+
+    init {
+        // Note: このView でタップイベントを奪い、下に配置したView に伝搬しないようにする
+        isClickable = true
+        isFocusable = true
+
+        setBackgroundResource(R.color.app_transparent_shadow)
+
+        // ローディングの配置
+        addView(
+            CircularProgressIndicator(context).apply {
+                isIndeterminate = true
+                setIndicatorColor(resources.getColor(R.color.app_accent, null))
+            },
+            LayoutParams(
+                WRAP_CONTENT,
+                WRAP_CONTENT,
+                Gravity.CENTER,
+            ),
+        )
+    }
+}
