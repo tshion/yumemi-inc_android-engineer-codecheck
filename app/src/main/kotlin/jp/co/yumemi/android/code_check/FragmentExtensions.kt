8a46e56cc00878e5bf06fd7b5d59ef5b2b0b2631
tshion <tenta_shion@yahoo.co.jp>
2023-11-22 01:03:29 +0900

    実装ブラッシュアップ

--- app/src/main/kotlin/jp/co/yumemi/android/code_check/FragmentExtensions.kt
+++
@@ -1,8 +0,0 @@
-package jp.co.yumemi.android.code_check
-
-import androidx.fragment.app.Fragment
-
-/**
- * 検索フローの取得
- */
-fun Fragment.searchUseCase() = (requireActivity().application as MainApplication).searchUseCase
