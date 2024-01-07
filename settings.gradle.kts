3b29adfd19839be48b43e2586eb6536c6d19c672
tshion <tenta_shion@yahoo.co.jp>
2024-01-07 17:35:52 +0900

    依存関係の整備

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -4,6 +4,14 @@ pluginManagement {
         mavenCentral()
         gradlePluginPortal()
     }
+    resolutionStrategy {
+        eachPlugin {
+            // 参考文献: https://github.com/google/play-services-plugins/issues/223#issuecomment-1236159132
+            if (requested.id.id == "com.google.android.gms.oss-licenses-plugin") {
+                useModule("com.google.android.gms:oss-licenses-plugin:${requested.version}")
+            }
+        }
+    }
 }
 dependencyResolutionManagement {
     repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
