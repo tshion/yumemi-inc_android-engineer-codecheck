49e09ea52ac25a891a95bdef328ad2ad2b176be2
tshion <tenta_shion@yahoo.co.jp>
2024-04-25 17:33:54 +0900

    コメントの追記

--- .github/scripts/can-release.bash
+++ .github/scripts/can-release.bash
@@ -1,5 +1,10 @@
 #!/bin/bash
 
+# 引数で指定されたGit タグ名でリリースできるかどうか
+# ※GitHub CLI のアクセス権限等の設定が必要
+#
+# $1 -> Git タグ名
+
 if gh release view $1 > /dev/null; then
   echo "$1 is already released!"
   exit 1
